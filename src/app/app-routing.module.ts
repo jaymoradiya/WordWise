882296e30dfbc2291de68e5c9efa5d30f8ee277a
Jay Moradiya <jaymoradiya0@gmail.com>
2023-02-23 19:10:00 +0530

    auth guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,11 @@ import { AnswerComponent } from './pages/how-it-works/answer/answer.component';
 import { HowItWorksComponent } from './pages/how-it-works/how-it-works.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { TranslateContainerComponent } from './pages/translate-container/translate-container.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {path: "", component: HomePageComponent},
-  {path: "translate-it", component: TranslateContainerComponent},
+  {path: "translate-it", component: TranslateContainerComponent, canActivate: [AuthGuard]},
   {path: "questions", component: HowItWorksComponent,
   children: [
     {path: ":id", component: AnswerComponent},
